d5d4b038075efce11e099dbe518a72b101f7a881
Lucas Legrand <legrand.lucas0@gmail.com>
2024-12-28 01:03:06 +0100

    Fix module structure and improve code organization

---
+++ src/content.js
@@ -0,0 +1,26 @@
+(function() {
+    function initializeControls() {
+        if (YTManager.utils.isWatchLaterPage()) {
+            YTManager.watchLater.addWatchLaterControls();
+        } else if (YTManager.utils.isSubscriptionsPage()) {
+            YTManager.subscriptions.addSubscriptionControls();
+        }
+    }
+
+    let initAttempts = 0;
+    const maxAttempts = 10;
+    const interval = setInterval(() => {
+        if (document.querySelector('ytd-guide-section-renderer')) {
+            initializeControls();
+            clearInterval(interval);
+        }
+        initAttempts++;
+        if (initAttempts >= maxAttempts) {
+            clearInterval(interval);
+        }
+    }, 1000);
+
+    window.addEventListener('yt-navigate-finish', () => {
+        setTimeout(initializeControls, 1000);
+    });
+})();
